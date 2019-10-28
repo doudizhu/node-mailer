8aed81ad11dabeee03fc96e249812c2f3a48bf50
lipeng <lipeng@daokoudai.com>
2019-10-28 14:32:12 +0800

    3.Nodemailer实战-实现邮箱发送的三部曲
    step 1 通过哪个邮箱服务发送
    step 2 发送内容配置
    step 3 使用工具发送

--- server.js
+++ server.js
@@ -28,7 +28,6 @@ app.get('/',(req,res)=>{
 })
 
 app.post('/addUser',(req,res)=>{
-  // console.log(req.body)
   User.findOne({username:req.body.username})
     .then(user=>{
       if(user){
@@ -53,6 +52,56 @@ app.post('/addUser',(req,res)=>{
       }
     })
 })
+
+// app.post('/retrievePwd', (req, res) => {
+//   User.findOne({ email: req.body.email }).then(user => {
+//     console.log(user);
+//   });
+// });
+app.post('/retrievePwd', (req, res) => {
+  User.findOne({ username: req.body.username }).then(user => {
+    if(!user){
+      res.status(400).json({
+        state: 'failed',
+        msg: '该用户不存在',
+      })
+    }else{
+      // step 1 通过哪个邮箱服务发送
+      let transporter = nodemailer.createTransport({
+        service: 'qq',
+        secure: true,
+        auth:{
+          user: '3563387158@qq.com', // 使用的邮箱
+          pass: 'uruatupakrcwchde', // IMAP/SMTP服务
+        },
+      })
+      // step 2 发送内容配置
+      let mailOptions = {
+        from: '3563387158@qq.com', // 使用的邮箱
+        // to:req.body.email,
+        to: user.email, // 发送至邮箱
+        // cc:'抄送',
+        // bcc:'密送',
+        subject:'找回密码',
+        text:`你的用户名：${user.username}，密码：${user.pwd}`,
+      }
+      // step 3 使用工具发送
+      transporter.sendMail(mailOptions, (err, data) => {
+        if(err){
+          res.status(400).json({
+            state: 'failed',
+            msg: err,
+          })
+        }else{
+          res.status(200).json({
+            state:'suc',
+            msg:`密码已发送至您的邮箱：${req.body.email}`
+          })
+        }
+      })
+    }
+  });
+});
 
 const port = process.env.PORT || 5000
 
